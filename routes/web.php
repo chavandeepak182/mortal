b29d7fb050c353154382dc760c7d466144fd758b
Vaibhav Saxena <shubhav260397@gmail.com>
2024-11-15 18:42:11 +0530

    New UI Code

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontend.index');
 });
-
-Route::get('/index', function () {
-    return view('frontend.index-2');
-});
-
-Route::get('/services', function () {
-    return view('frontend.services');
-});
